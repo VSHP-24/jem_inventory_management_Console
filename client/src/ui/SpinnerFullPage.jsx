069e1c9bcf3c576d80a405c99b7b36ded42673a9
VSHP-24 <srihariprasadsniper@gmail.com>
2024-12-12 18:49:03 +0530

    BUILD: Created Spinner for Full Page

---
+++ client/src/ui/SpinnerFullPage.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import Spinner from "./Spinner";
+
+const StyledSpinnerFullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-gold-200);
+  width: 100%;
+`;
+
+function SpinnerFullPage() {
+  return (
+    <StyledSpinnerFullPage>
+      <Spinner />
+    </StyledSpinnerFullPage>
+  );
+}
+
+export default SpinnerFullPage;
