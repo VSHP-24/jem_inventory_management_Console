4383e9555053389af1be0a6969094f9e91fb6f55
VSHP-24 <srihariprasadsniper@gmail.com>
2024-12-12 19:16:36 +0530

    FEAT: Implemented Lazy Loading and Code Splitting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,27 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 import GlobalStyles from "./styles/GlobalStyles";
-import AppLayout from "./ui/AppLayout";
-import Dashboard from "./pages/Dashboard";
-import Products from "./pages/Products";
-import PageNotFound from "./pages/PageNotFound";
-import Create from "./pages/Create";
-import Manage from "./pages/Manage";
-import Purchase from "./pages/Purchase";
-import Inventory from "./pages/Inventory";
-import Customer from "./pages/Customer";
-import Order from "./pages/Order";
-import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword";
 import ProtectedRoute from "./ui/ProtectedRoute";
-import ResetPassword from "./pages/ResetPassword";
-import Users from "./pages/Users";
+import AppLayout from "./ui/AppLayout";
+import SpinnerFullPage from "./ui/SpinnerFullPage";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Create = lazy(() => import("./pages/Create"));
+const Manage = lazy(() => import("./pages/Manage"));
+const Purchase = lazy(() => import("./pages/Purchase"));
+const Inventory = lazy(() => import("./pages/Inventory"));
+const Customer = lazy(() => import("./pages/Customer"));
+const Order = lazy(() => import("./pages/Order"));
+const Login = lazy(() => import("./pages/Login"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
+const ResetPassword = lazy(() => import("./pages/ResetPassword"));
+const Users = lazy(() => import("./pages/Users"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -34,32 +37,37 @@ function App() {
       <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
-        <Routes>
-          <Route
-            element={
-              <ProtectedRoute>
-                <AppLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate replace to="dashboard" />} />
-            <Route path="create" element={<Create />} />
-            <Route path="manage" element={<Manage />} />
-            <Route path="users" element={<Users />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="products" element={<Products />} />
-            <Route path="orders" element={<Order />} />
-            <Route path="customers" element={<Customer />} />
-            <Route path="purchases" element={<Purchase />} />
-            <Route path="inventory" element={<Inventory />} />
-          </Route>
+        <Suspense fallback={<SpinnerFullPage />}>
+          <Routes>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to="dashboard" />} />
+              <Route path="create" element={<Create />} />
+              <Route path="manage" element={<Manage />} />
+              <Route path="users" element={<Users />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="products" element={<Products />} />
+              <Route path="orders" element={<Order />} />
+              <Route path="customers" element={<Customer />} />
+              <Route path="purchases" element={<Purchase />} />
+              <Route path="inventory" element={<Inventory />} />
+            </Route>
 
-          <Route path="login" element={<Login />} />
-          <Route path="forgotPassword" element={<ForgotPassword />} />
-          <Route path="resetPassword/:resetToken" element={<ResetPassword />} />
+            <Route path="login" element={<Login />} />
+            <Route path="forgotPassword" element={<ForgotPassword />} />
+            <Route
+              path="resetPassword/:resetToken"
+              element={<ResetPassword />}
+            />
 
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
 
       <Toaster
--- client/src/ui/Spinner.jsx
+++ client/src/ui/Spinner.jsx
@@ -16,7 +16,7 @@ const Spinner = styled.div`
   border-radius: 50%;
   background: radial-gradient(farthest-side, var(--color-grey-600) 94%, #0000)
       top/10px 10px no-repeat,
-    conic-gradient(#0000 30%, var(--color-grey-600));
+    conic-gradient(#0000 30%, var(--color-grey-900));
   mask: radial-gradient(farthest-side, #0000 calc(100% - 10px), #000 0);
   animation: ${rotate} 1.5s infinite linear;
 `;
