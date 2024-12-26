b64bff9367f4599adc0d97d5ff0e0054962ba69f
VSHP-24 <srihariprasadsniper@gmail.com>
2024-12-26 16:14:12 +0530

    FEAT: Created Controller , Model and Routes for category

--- server/models/categoryModel.js
+++ server/models/categoryModel.js
@@ -9,17 +9,35 @@ const categorySchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: [true, "A category must have a name"],
       unique: true,
       trim: true,
     },
     slug: String,
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
   }
 );
+
+// THIS GETS ALL THE SUBCATEGORIES UNDER THE CATEGORY
+categorySchema.virtual("subCategories", {
+  ref: "SubCategory",
+  foreignField: "category",
+  localField: "_id",
+});
+
+// THIS GETS ALL THE PRODUCTS UNDER THE CATEGORY
+categorySchema.virtual("products", {
+  ref: "Product",
+  foreignField: "category",
+  localField: "_id",
+});
 
 categorySchema.pre("save", function (next) {
   this.slug = slugify(this.name, { replacement: "-", lower: true, trim: true });
