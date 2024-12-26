e2b20bdc6c37140e7edee1a5a04d9a3642dbe63e
VSHP-24 <srihariprasadsniper@gmail.com>
2024-12-26 16:19:55 +0530

    FEAT: Created Controller , Model and Routes for parts

--- server/routes/partRoutes.js
+++ server/routes/partRoutes.js
@@ -6,10 +6,19 @@ import {
   updatePart,
   deletePart,
 } from "../controllers/partController.js";
+import { protect, restrictTo } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.route("/").get(getAllParts).post(createPart);
-router.route("/:id").get(getPart).patch(updatePart).delete(deletePart);
+// THESE ROUTES ARE AVAILABLE WITHOUT ANY AUTH
+router.route("/").get(getAllParts);
+router.route("/:id").get(getPart);
+
+// PROTECTS ALL ROUTES AFTER THIS MIDDLEWARE AND RESTRICTS TO ADMIN
+router.use(protect, restrictTo("admin"));
+
+// THESE ROUTES ARE PROTECTED AND RESTRICTS TO ONLY ADMIN
+router.route("/").post(createPart);
+router.route("/:id").patch(updatePart).delete(deletePart);
 
 export default router;
