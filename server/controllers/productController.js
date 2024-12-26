e9074807ddd341e475ff196cfd8c86dc9018aae5
VSHP-24 <srihariprasadsniper@gmail.com>
2024-12-26 16:21:22 +0530

    FEAT: Created Controller , Model and Routes for products

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,48 +1,29 @@
 /*eslint no-unused-vars: ["error", { "argsIgnorePattern": "req|res|next|val" }]*/
 
 import Product from "./../models/productModel.js";
-import catchAsync from "../utils/catchAsync.js";
-import AppError from "../utils/appError.js";
-import { createOne, deleteOne, updateOne } from "./handlerFactory.js";
+import {
+  createOne,
+  deleteOne,
+  getAll,
+  getOne,
+  updateOne,
+} from "./handlerFactory.js";
 
-/////////////////////////////////////////////////
-//           FETCHES ALL THE PRODUCTS
-/////////////////////////////////////////////////
+export const getAllProducts = getAll(Product, [
+  { path: "brand", select: "id name isDeleted slug" },
+  { path: "model", select: "id name isDeleted slug" },
+  { path: "category", select: "id name isDeleted slug" },
+  { path: "subCategory", select: "id name isDeleted slug" },
+  { path: "includedParts.part", select: "id name isDeleted slug" },
+]);
 
-export const getAllProducts = catchAsync(async (req, res, next) => {
-  const products = await Product.find();
-  res.status(200).json({
-    status: "success",
-    results: products.length,
-    data: {
-      products,
-    },
-  });
-});
-
-/////////////////////////////////////////////////
-//           FETCHES SPECIFIC PRODUCT
-/////////////////////////////////////////////////
-
-export const getProduct = catchAsync(async (req, res, next) => {
-  const product = await Product.findById(req.params.id);
-
-  if (!product) {
-    return next(
-      new AppError(
-        "No product was found with that ID. Please check the ID again",
-        404
-      )
-    );
-  }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      product,
-    },
-  });
-});
+export const getProduct = getOne(Product, [
+  { path: "brand", select: "id name isDeleted slug" },
+  { path: "model", select: "id name isDeleted slug" },
+  { path: "category", select: "id name isDeleted slug" },
+  { path: "subCategory", select: "id name isDeleted slug" },
+  { path: "includedParts.part", select: "id name isDeleted slug" },
+]);
 
 export const createProduct = createOne(Product);
 export const updateProduct = updateOne(Product);
