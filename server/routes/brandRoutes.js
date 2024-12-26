a1cf15d552e836de95461976e5da6c92a4b0fdcb
VSHP-24 <srihariprasadsniper@gmail.com>
2024-12-26 16:12:07 +0530

    FEAT: Created Controller , Model and Routes for brand

--- server/routes/brandRoutes.js
+++ server/routes/brandRoutes.js
@@ -6,10 +6,19 @@ import {
   updateBrand,
   deleteBrand,
 } from "../controllers/brandController.js";
+import { protect, restrictTo } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.route("/").get(getAllBrands).post(createBrand);
-router.route("/:id").get(getBrand).patch(updateBrand).delete(deleteBrand);
+// THESE ROUTES ARE AVAILABLE WITHOUT ANY AUTH
+router.route("/").get(getAllBrands);
+router.route("/:id").get(getBrand);
+
+// PROTECTS ALL ROUTES AFTER THIS MIDDLEWARE AND RESTRICTS TO ADMIN
+router.use(protect, restrictTo("admin"));
+
+// THESE ROUTES ARE PROTECTED AND RESTRICTS TO ONLY ADMIN
+router.route("/").post(createBrand);
+router.route("/:id").patch(updateBrand).delete(deleteBrand);
 
 export default router;
