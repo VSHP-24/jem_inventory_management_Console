5ac68da258afbd7084471f9ba648c0ce40e1f8fb
VSHP-24 <srihariprasadsniper@gmail.com>
2025-01-28 17:43:46 +0530

    BUILD: Created a Read Operation to get orders filtered for logged-in customer

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -5,14 +5,20 @@ import {
   getOrder,
   updateOrder,
   deleteOrder,
+  getAllOrdersByCustomer,
 } from "../controllers/orderController.js";
 import { protect, restrictTo } from "../controllers/authController.js";
-import { setUserId } from "../controllers/userController.js";
+import { getMe, setUserId } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // PROTECTS ALL ROUTES AFTER THIS MIDDLEWARE
 router.use(protect);
+
+// THIS ROUTE IS PROTECTED AND FILTERS ORDERS BY CUSTOMER ID
+router
+  .route("/myOrders")
+  .get(restrictTo("customer"), getMe, getAllOrdersByCustomer);
 
 // THESE ROUTES ARE PROTECTED
 router.route("/:id").get(getOrder).patch(updateOrder).delete(deleteOrder);
